09f8b52c115c2288a1195038044714c87571cba9
abhishek199677 <108387488+abhishek199677@users.noreply.github.com>
2024-10-14 16:01:50 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
-STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
